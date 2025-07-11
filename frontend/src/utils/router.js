f95bc63dfbd495ada7c8acd59fe234437e4858dd
Abdulqader-1994 <a.q.m1994@gmail.com>
2025-07-11 09:41:56 +0300

    Update router.js

--- frontend/src/utils/router.js
+++ frontend/src/utils/router.js
@@ -2,6 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Terms from '../views/terms.vue'
 import Privacy from '../views/privacy.vue'
+import Chat from '../views/chat.vue'
+import SignIn from '../views/signin.vue'
+import SignUp from '../views/signup.vue'
+
+const authed = () => {
+  const appStorage = JSON.parse(localStorage.getItem('appStorage'))
+  return (appStorage && appStorage.user) ? true : false;
+}
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +24,25 @@ const router = createRouter({
     { path: '/', name: 'home', component: HomeView },
     { path: '/terms', name: 'terms', component: Terms },
     { path: '/privacy', name: 'privacy', component: Privacy },
+    {
+      path: '/signin',
+      name: 'signin',
+      component: SignIn,
+      beforeEnter: (to, from, next) => authed() ? next('/chat') : next()
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignUp,
+      beforeEnter: (to, from, next) => authed() ? next('/chat') : next()
+    },
+    {
+      path: '/chat',
+      name: 'chat',
+      component: Chat,
+      beforeEnter: (to, from, next) => authed() ? next() : next('/signin')
+    },
+
     // Redirect any undefined route to the home page
     { path: '/:pathMatch(.*)*', redirect: '/' },
   ],
